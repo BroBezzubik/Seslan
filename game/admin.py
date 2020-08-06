2ed4f9d4248c9153e7c652171605f2decd7ae567
BroBezzubik <Barsik98n@mail.ru>
2020-08-06 10:37:45 +0300

    Добавлена базовая настройка игры

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.postgres import fields
+from django_json_widget.widgets import JSONEditorWidget
+from .models import Log, Game, Proffesion, Game_event_image, Game_event, Player
 
-# Register your models here.
+@admin.register(Log, Game, Proffesion, Game_event_image, Game_event, Player)
+class SeslanAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        fields.JSONField: {'widget': JSONEditorWidget}
+    }
+
--- game/models.py
+++ game/models.py
@@ -6,36 +6,55 @@ from django.contrib.postgres import fields as post_fields
 
 class Log(models.Model):
     file_name = models.CharField(max_length=50, null=False, verbose_name="Log file Name")
-    file_field = models.FileField(upload_to='Game logs/%Y/%m/%d/')
+    file_path = models.CharField(default="Logs/",max_length=300, null=False)
+
+    def __str__(self):
+        return self.file_name
 
 
 class Game(models.Model):
+    name = models.CharField(default="Seslan", max_length=30)
     date = models.DateTimeField(auto_now=False, auto_now_add=False, verbose_name="Date of Game")
     description = models.TextField(max_length=1000, verbose_name="Game description")
     log_id = models.OneToOneField(Log, on_delete=models.PROTECT)
+    game_time = models.FloatField(default=0, null=False, verbose_name='In game time')
+
+    def __str__(self):
+        return self.name
 
 
 class Proffesion(models.Model):
-    proffesion_name = models.CharField(max_length=30, verbose_name=Proffesion)
+    proffesion_name = models.CharField(max_length=30, verbose_name='Proffesion')
     description = models.TextField(max_length=500)
 
+    def __str__(self):
+        return self.proffesion_name
 
-class Object_image(models.Model):
+
+class Game_event_image(models.Model):
     file_name = models.CharField(max_length=30, verbose_name="Name of object image")
-    file_field = models.FileField('Models/')
+    file_field = models.FileField(upload_to='Models')
+
+    def __str__(self):
+        return self.file_name
 
 
-class Object(models.Model):
-    image = models.ForeignKey(Object_image, on_delete=models.PROTECT)
+class Game_event(models.Model):
+    name = models.CharField(default="Some object", max_length=20)
+    image = models.ForeignKey(Game_event_image, on_delete=models.PROTECT)
     information = post_fields.JSONField()
+    pos_x = models.IntegerField(default=0)
+    pox_y = models.IntegerField(default=0)
 
-
+    def __str__(self):
+        return self.name
+  
 class Player(models.Model):
     game_id = models.ForeignKey(Game, on_delete=models.PROTECT)
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     proffesion = models.OneToOneField(Proffesion, on_delete=models.PROTECT)
     permissions = post_fields.JSONField()
-    objects = models.ManyToManyField(Object)
+    visible_objects = models.ManyToManyField(Game_event)
 
 
 
